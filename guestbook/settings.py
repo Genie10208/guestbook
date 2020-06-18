6032ebbcaf1ed56d48bc5b655e63991721f59a6e
10208 <jewg2016@gmail.com>
2020-06-18 11:32:20 +0800

    使用者登入登出

--- guestbook/settings.py
+++ guestbook/settings.py
@@ -30,14 +30,15 @@ ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
-INSTALLED_APPS = [
+INSTALLED_APPS = [ 
+    'veb',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'veb',
+   
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = "/"
